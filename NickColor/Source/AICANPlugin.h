d88ce2389e56522fe020e4e061c1d21a717a4256
Thijs Alkemade <thijsalkemade@gmail.com>
2011-07-11 15:12:03 +0200

    Basic version done now. :)

---
+++ NickColor/Source/AICANPlugin.h
@@ -0,0 +1,17 @@
+//
+//  AICANPlugin.h
+//  ColorAllNames
+//
+//  Created by Thijs Alkemade on 10-06-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface AICANPlugin : NSObject {
+@private
+    
+}
+
+@end
---
+++ NickColor/Source/AICANPlugin.m
@@ -0,0 +1,29 @@
+//
+//  AICANPlugin.m
+//  ColorAllNames
+//
+//  Created by Thijs Alkemade on 10-06-11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AICANPlugin.h"
+
+
+@implementation AICANPlugin
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
